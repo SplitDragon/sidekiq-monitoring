82474d0bd0448c45512d6f863f9a10cdb676e0ec
Esteban <esteban@splitdragon.com>
2020-05-01 15:13:31 +0200

    Build

--- build-push.sh
+++ build-push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# docker build . -t asia.gcr.io/splitdragon/sidekiq-mon:2.1
 docker build . -t asia.gcr.io/splitdragon/sidekiq-mon:latest
 docker push asia.gcr.io/splitdragon/sidekiq-mon:latest
