b7073dba432b4197ad83f5254fec37d40bf9cc8a
Esteban <esteban@splitdragon.com>
2020-05-01 15:10:55 +0200

    Relaod queeues from time to time

--- sidekiq-monitoring.rb
+++ sidekiq-monitoring.rb
@@ -1,25 +1,5 @@
 require "sidekiq/monitor"
-
 include Sidekiq
-
-def sets
-  [
-    Sidekiq::RetrySet.new,
-    Sidekiq::DeadSet.new,
-    Sidekiq::ScheduledSet.new,
-    Sidekiq::DeadSet.new,
-  ]
-end
-
-def queues
-  [
-    :hipri,
-    :default,
-    :lowpri,
-    :mailer,
-    :crawler,
-  ]
-end
 
 class MyLogger
   def initialize
@@ -30,41 +10,70 @@ class MyLogger
   end
 end
 
-def logger
-  MyLogger.new
-end
+class SidekiqMonitoring
 
-def print_monitoring_data
-  queues.each do |q|
-    size = Sidekiq::Queue.new(q).size
-    logger.info "sdmon_q_size_#{q.to_s}: #{size}"
+  def initialize queues, sleep_interval
+    @queues         = queues
+    @sleep_interval = sleep_interval
   end
 
-  sets.each do |s|
-    size = s.size
-    name = s.name
-    logger.info "sdmon_q_size_#{name}: #{size}"
+  def sets
+    [
+      Sidekiq::RetrySet.new,
+      Sidekiq::DeadSet.new,
+      Sidekiq::ScheduledSet.new,
+      Sidekiq::DeadSet.new,
+    ]
   end
 
-  logger.info "sdmon_q_size_all: #{Sidekiq::Stats.new.enqueued}"
+  def logger
+    MyLogger.new
+  end
 
-  busy_count = Sidekiq::ProcessSet.new.map { |x| x["busy"] }.reduce(&:+)
-  logger.info "sdmon_proc_busy: #{busy_count}"
+  def print_monitoring_data
+    # Log each queue data
+    @queues.each do |q|
+      size    = Sidekiq::Queue.new(q).size
+      latency = Sidekiq::Queue.new(q).latency
 
-  logger.info "sdmon_proc_all: #{Sidekiq::ProcessSet.new.size}"
-  logger.info "sdmon_proc_workers: #{Sidekiq::Workers.new.size}"
-  $stdout.flush
-end
+      logger.info "sdmon_q_size_#{q.to_s}: #{size}"
+      logger.info "sdmon_q_lat_#{q.to_s}: #{latency}"
+    end
 
-def monitoring_loop
-  sleep_time = ENV["SIDEKIQ_MON_SLEEP_INTERVAL"]&.to_i || 5
+    # Log predefined sets data
+    sets.each do |s|
+      size    = s.size
+      name    = s.name
+      logger.info "sdmon_q_size_#{name}: #{size}"
+    end
 
-  loop do
-    print_monitoring_data
+    # Log all queues combined data
+    logger.info "sdmon_q_size_all: #{Sidekiq::Stats.new.enqueued}"
 
-    sleep sleep_time
-    break if sleep_time.zero?
+    # Log currently processing information data
+    busy_count = Sidekiq::ProcessSet.new.map { |x| x["busy"] }.reduce(&:+)
+    logger.info "sdmon_proc_busy: #{busy_count}"
+
+    # Log additional Sidekiq Processes and Workers data
+    logger.info "sdmon_proc_all: #{Sidekiq::ProcessSet.new.size}"
+    logger.info "sdmon_proc_workers: #{Sidekiq::Workers.new.size}"
+    $stdout.flush
+  end
+
+  def monitoring_loop count = 180
+    count.times do
+      print_monitoring_data
+
+      sleep @sleep_interval
+      break if @sleep_interval.zero?
+    end
   end
 end
 
-monitoring_loop
+loop do
+  loop_count     = ENV['LOOP_COUNT']&.to_i || 180
+  queues         = ENV['SIDEKIQ_MON_QUEUES']&.split(',') || Sidekiq::Queue.all.map(&:name)
+  sleep_interval = ENV["SIDEKIQ_MON_SLEEP_INTERVAL_SECS"]&.to_f || 5
+
+  SidekiqMonitoring.new(queues, sleep_interval).monitoring_loop loop_count
+end
